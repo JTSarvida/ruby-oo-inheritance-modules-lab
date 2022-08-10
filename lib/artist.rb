df81fc016e27bf29c8e8e75b6660fca4447daeca
jtsarvida <jtsarvida@gmail.com>
2022-08-09 20:40:28 -0400

    commit tree d7f3fbccc887e4d2da5a6d17abfd2f7cd7321a58

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,7 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 
 class Artist
   attr_accessor :name
@@ -6,25 +9,20 @@ class Artist
 
   @@artists = []
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable
+  include Memorable::InstanceMethods
+
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +34,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
